9912b1efcdb0b0fef13792fd87287fa5a5bab563
burak <57973@etu.he2b.be>
2022-03-24 11:13:27 +0100

    Créér la classe javadoc. (Je vais faire la javadoc de toutes mes methodes plus tard)

--- echecs-57973g/src/main/java/g57973/chess/model/Player.java
+++ echecs-57973g/src/main/java/g57973/chess/model/Player.java
@@ -10,5 +10,16 @@ package g57973.chess.model;
  * @author g57973
  */
 public class Player {
+    private Color color;
+
+    public Player(Color color) {
+        this.color = color;
+    }
+    
+    public Color getColor(){
+        return this.color;
+    }
+    
+    
     
 }
