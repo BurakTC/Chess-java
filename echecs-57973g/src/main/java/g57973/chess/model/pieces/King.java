f202cb0fb33185d215d0c2d4a4a533b62c75e02b
Burak <Burak@192.168.26.105>
2022-04-24 16:53:38 +0200

    Ajout classe ROI , FAIRE TESTS

---
+++ echecs-57973g/src/main/java/g57973/chess/model/pieces/King.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g57973.chess.model.pieces;
+
+import g57973.chess.model.Board;
+import g57973.chess.model.Color;
+import g57973.chess.model.Direction;
+import g57973.chess.model.Position;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Burak
+ */
+public class King extends Piece {
+
+    public King(Color color) {
+        super(color);
+    }
+
+    @Override
+    List<Position> getPossibleMoves(Position position, Board board) {
+        List<Position> possibleMoves = new ArrayList();
+        List<Position> testMoves = new ArrayList<>();
+        testMoves.add(position.next(Direction.N));
+        testMoves.add(position.next(Direction.NW));
+        testMoves.add(position.next(Direction.W));
+        testMoves.add(position.next(Direction.SW));
+        testMoves.add(position.next(Direction.S));
+        testMoves.add(position.next(Direction.SE));
+        testMoves.add(position.next(Direction.E));
+        testMoves.add(position.next(Direction.NE));
+        
+        testMoves.stream().filter(pos -> (board.contains(pos) && board.isFree(pos)||board.containsOppositeColor(pos, this.getColor()))).forEachOrdered(pos -> {
+            possibleMoves.add(pos);
+        });
+        return possibleMoves;
+                
+
+    }
+    
+}
