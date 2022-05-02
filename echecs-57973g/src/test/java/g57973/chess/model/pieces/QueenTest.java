5224d1a5972b1163218acfae1840c4925d3c07cd
burak <57973@etu.he2b.be>
2022-05-02 15:42:50 +0200

    Modifs et tests de Queen

--- echecs-57973g/src/test/java/g57973/chess/model/pieces/QueenTest.java
+++ echecs-57973g/src/test/java/g57973/chess/model/pieces/QueenTest.java
@@ -36,7 +36,7 @@ public class QueenTest {
 
     @Test
     public void testGetPossibleMovesQueenLIBRE() {
-        Position position = new Position(3,3);
+        Position position = new Position(3, 3);
         Piece piece = new Queen(Color.WHITE);
         board.setPiece(piece, position);
         List<Position> expected = List.of(
@@ -45,96 +45,147 @@ public class QueenTest {
                 new Position(2, 3),
                 new Position(4, 3),
                 new Position(5, 3),
-                new Position(6,3),
-                new Position(7,3),
-
-                new Position(3,0),
-                new Position(3,1),
-                new Position(3,2),
-                new Position(3,4),
-                new Position(3,5),
-                new Position(3,6),
-                new Position(3,7),
-                
-                 new Position(0,0),
-                new Position(1,1),
-                new Position(2,2),
-                new Position(4,4),
-                new Position(5,5),
-                new Position(6,6),
-                new Position(7,7),
-                
-                 new Position(0,6),
-                new Position(1,5),
-                new Position(2,4),
-                new Position(4,2),
-                new Position(5,1),
-                new Position(6,0)
-                );
+                new Position(6, 3),
+                new Position(7, 3),
+                new Position(3, 0),
+                new Position(3, 1),
+                new Position(3, 2),
+                new Position(3, 4),
+                new Position(3, 5),
+                new Position(3, 6),
+                new Position(3, 7),
+                new Position(0, 0),
+                new Position(1, 1),
+                new Position(2, 2),
+                new Position(4, 4),
+                new Position(5, 5),
+                new Position(6, 6),
+                new Position(7, 7),
+                new Position(0, 6),
+                new Position(1, 5),
+                new Position(2, 4),
+                new Position(4, 2),
+                new Position(5, 1),
+                new Position(6, 0)
+        );
         List<Position> positions = piece.getPossibleMoves(position, board);
         assertEqualsIgnoringOrder(expected, positions);
     }
 
     @Test
-    public void testGetPossibleMovesKingCOIN() {
+    public void testGetPossibleMovesQueenCOIN() {
         Position position = new Position(0, 0);
-        Piece piece = new King(Color.WHITE);
+        Piece piece = new Queen(Color.WHITE);
         board.setPiece(piece, position);
-
+        
         List<Position> expected = List.of(
-                new Position(1, 1),
-                new Position(1, 0),
-                new Position(0, 1)
+                new Position(1,0),
+                new Position(2,0),
+                new Position(3,0),
+                new Position(4,0),
+                new Position(5,0),
+                new Position(6,0),
+                new Position(7,0),
+                
+                new Position(1,1),
+                new Position(2,2),
+                new Position(3,3),
+                new Position(4,4),
+                new Position(5,5),
+                new Position(6,6),
+                new Position(7,7),
+                
+                new Position(0,1),
+                new Position(0,2),
+                new Position(0,3),
+                new Position(0,4),
+                new Position(0,5),
+                new Position(0,6),
+                new Position(0,7)
         );
-        List<Position> positions = piece.getPossibleMoves(position, board);
+        List<Position> positions
+                = piece.getPossibleMoves(position, board);
         assertEqualsIgnoringOrder(expected, positions);
     }
-    
-     @Test
-    public void testGetPossibleMovesKingPAWNENNEMI() {
-        Position position = new Position(3, 4);
-        Position posennemi = new Position(4,4);
+
+    @Test
+    public void testGetPossibleMovesQueenPAWNENNEMI() {
+        Position position = new Position(3, 3);
+        Position posennemi = new Position(4, 4);
         Piece ennemi = new Pawn(Color.BLACK);
-        Piece piece = new King(Color.WHITE);
+        Piece piece = new Queen(Color.WHITE);
         board.setPiece(piece, position);
         board.setPiece(ennemi, posennemi);
 
         List<Position> expected = List.of(
-                new Position(2, 3),
+                new Position(0,3),
+                new Position(1,3),
+                new Position(2,3),
+                new Position(4,3),
+                new Position(5,3),
+                new Position(6,3),
+                new Position(7,3),
+                new Position(3, 0),
+                new Position(3, 1),
+                new Position(3, 2),
+                new Position(3, 4),
+                new Position(3, 5),
+                new Position(3, 6),
+                new Position(3, 7),
+                new Position(0, 0),
+                new Position(1, 1),
+                new Position(2, 2),
+                new Position(6,0),
+                new Position(5,1),
+                new Position(4, 2),
                 new Position(2, 4),
-                new Position(2, 5),
-                new Position(3, 3),
-                new Position(4, 3),
-                new Position(4, 4),
-                new Position(4, 5),
-                new Position(3, 5)
+                new Position(1,5),
+                new Position(0, 6),
+                new Position(4,4)  //On rajoute la position de l'ennemi trouvé dans la liste
         );
         List<Position> positions = piece.getPossibleMoves(position, board);
         assertEqualsIgnoringOrder(expected, positions);
     }
-    
-      @Test
-    public void testGetPossibleMovesKingPAWNAMI() {
-        Position position = new Position(3, 4);
+
+    @Test
+    public void testGetPossibleMovesQueenPAWNAMI() {
+        Position position = new Position(3,3);
         Position posallié = new Position(4,4);
         Piece allié = new Pawn(Color.WHITE);
-        Piece piece = new King(Color.WHITE);
+        Piece piece = new Queen(Color.WHITE);
         board.setPiece(piece, position);
         board.setPiece(allié, posallié);
 
         List<Position> expected = List.of(
-                new Position(2, 3),
+                new Position(0,3),
+                new Position(1,3),
+                new Position(2,3),
+                new Position(4,3),
+                new Position(5,3),
+                new Position(6,3),
+                new Position(7,3),
+                new Position(3, 0),
+                new Position(3, 1),
+                new Position(3, 2),
+                new Position(3, 4),
+                new Position(3, 5),
+                new Position(3, 6),
+                new Position(3, 7),
+                new Position(0, 0),
+                new Position(1, 1),
+                new Position(2, 2),
+                new Position(6,0),
+                new Position(5,1),
+                new Position(4, 2),
                 new Position(2, 4),
-                new Position(2, 5),
-                new Position(3, 3),
-                new Position(4, 3),
-                new Position(4, 5),
-                new Position(3, 5)
+                new Position(1,5),
+                new Position(0, 6)
+                
         );
-        List<Position> positions = piece.getPossibleMoves(position, board);
+        List<Position> positions
+                = piece.getPossibleMoves(position, board);
         assertEqualsIgnoringOrder(expected, positions);
     }
-    
 
     /*
      *      Permet de tester si deux listes de positions sont identiques à l'ordre
